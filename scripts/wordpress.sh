645aed9796cd7c9ae2a02f84cf5b23d74f0b630f
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-04-20 13:59:00 +0300

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/wordpress.sh
+++ scripts/wordpress.sh
@@ -36,8 +36,8 @@ for d in juju mysql wordpress; do
     lxc-attach -n $d -- /usr/sbin/service ssh restart
     lxc-attach -n $d -- mkdir -p /home/ubuntu/.ssh/
     cat /root/.ssh/juju.pub > /var/lib/lxc/$d/delta0/home/ubuntu/.ssh/authorized_keys
-    grep -q "lxc.start.auto = 1" /var/lib/lxc/$d/config || echo "lxc.start.auto = 1" >> /var/lib/lxc/$d/config
-    grep -q "lxc.start.delay = 5" /var/lib/lxc/$d/config || echo "lxc.start.delay = 5" >> /var/lib/lxc/$d/config
+#    grep -q "lxc.start.auto = 1" /var/lib/lxc/$d/config || echo "lxc.start.auto = 1" >> /var/lib/lxc/$d/config
+#    grep -q "lxc.start.delay = 5" /var/lib/lxc/$d/config || echo "lxc.start.delay = 5" >> /var/lib/lxc/$d/config
 done
 
 mkdir -p /home/ubuntu/.ssh/
